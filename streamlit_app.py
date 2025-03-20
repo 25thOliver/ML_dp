5577d5b1a74b78bf2dc16648d8c5206ff60377e8
Sami <149001573+25thOliver@users.noreply.github.com>
2025-03-20 19:51:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Machine Learning App')
 
 st.write('This app builds a machine learning model!')
+
+df = pd.read_csv('penguins_cleaned.csv')
+df
